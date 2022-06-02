002b4a13884860542c3006ced675f9dbdfc38b40
Eduardo Teixeira <103132551+eduteixeirabhz@users.noreply.github.com>
2022-06-02 12:58:26 -0300

    Add files via upload

---
+++ Desafio010.py
@@ -0,0 +1,7 @@
+print('Bem vindo ao conversor digital de Real para Dólar e de Dólar para Real')
+n = float(input('Digite aqui quanto você possui em Reais: '))
+n1 = n / 5.56
+print('Você possui {} Dólares americanos'.format(n1))
+n2 = float(input('Digite agora quanto você possui em Dólares americanos: '))
+n3 = n2 * 5.56
+print('Você possui {} Reais'.format(n3))
